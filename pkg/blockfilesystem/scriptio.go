78de5aa1a2c710882b8c31f9c2487242473b613b
zcbm4321 <zcbm4321@gmail.com>
2023-12-05 14:18:10 +0900

    bug fix

--- pkg/blockfilesystem/scriptio.go
+++ pkg/blockfilesystem/scriptio.go
@@ -147,7 +147,7 @@ func (io *ScriptIoHandler) ReadScriptData(key []byte) (data []byte) {
 
 func (io *ScriptIoHandler) WriteScriptData(uniqKey, data []byte) (key []byte) {
 	prevMap := map[types.TxOutputType][]types.PrevOutputParam{}
-	prevMap[types.TxTypeScriptStore] = io.scriptTxPtr // for mapping
+	prevMap[types.TxTypeScript] = io.scriptTxPtr // for mapping
 
 	if ref := io.scriptIo.loadRefTx(uniqKey, io.toAddr); len(ref) != 0 {
 		prevMap[types.TxTypeScriptStore] = ref
